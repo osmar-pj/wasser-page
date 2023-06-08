b9ed51e7fa02431411e66729f7ef8feb79549c4b
Osmar Palomino <gunjop@Osmars-MacBook-Pro.local>
2023-06-08 11:37:18 -0500

    update address and location

---
+++ _next/static/lLrdpEFZwBGu26tbz6jP_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/163-ef46bb403db79ee3.js","static/css/6ece48a181204804.css","static/chunks/pages/index-182d882fa10c39c4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
